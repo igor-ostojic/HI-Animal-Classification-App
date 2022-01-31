4b2b42a8f3a1e01b8c8bca488000ee9b1be569d5
Igor Ostojic <igorostojic1996@gmail.com>
2022-01-31 23:31:50 +0100

    finished landing page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,30 @@
+//components
+
 //assets
-import Chicken from "../assets/chicken.svg";
+import Monkey from "../assets/animals-slideshow/monkey.svg";
 
 const Home = () => {
   return (
     <main className="home">
       <div className="home-wrapper">
         <div className="left-side">
-          <img src={Chicken} alt="Chicken" className="chicken" />
+          <img src={Monkey} alt="Animal" className="monkey" />
         </div>
         <div className="right-side">
           <h1>Animal Classifier App</h1>
           <p>Find out the species of any animal in just a few seconds.</p>
         </div>
       </div>
+      <p className="credits">
+        Made with <span>❤</span> by{" "}
+        <a
+          target="_blank"
+          href="https://github.com/igor-ostojic"
+          className="font-semibold hover:text-indigo-200"
+        >
+          Igor Ostojic
+        </a>
+      </p>
     </main>
   );
 };
